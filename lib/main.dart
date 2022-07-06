b3b40caad00f683b7f4419229696865f7019f7a3
daniellegmborges <daniellegmborges@gmail.com>
2022-07-06 11:07:01 -0300

    Inclusão da aula 71

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:test1/src/app_aula69_jokenPo.dart'; //para rodar no app, alterar para qual aula deve ser lida
+import 'package:test1/src/app_aula71_alcoolOuGasolina.dart'; //para rodar no app, alterar para qual aula deve ser lida
 
 void main () {
   runApp(
-    const AppAula69(), //para rodar no app, alterar para qual aula deve ser lida
+    const AppAula71(), //para rodar no app, alterar para qual aula deve ser lida
   );
 }
